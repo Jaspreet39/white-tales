cc7387cfb0a42f22eda9c3293f39075336e708b5
Jaspreet singh <jassingh0244@gmail.com>
2022-07-21 11:07:09 +0530

    last fix of margin

--- src/Component/BottomTop.js
+++ src/Component/BottomTop.js
@@ -1,52 +1,35 @@
-import React, { useEffect } from 'react'
-import '../Style/BottomTop.scss'
+import React, { useEffect } from "react";
+import "../Style/BottomTop.scss";
 import Aos from "aos";
-import 'aos/dist/aos.css'; 
+import "aos/dist/aos.css";
 
 function BottomTop() {
-
-
-
   useEffect(() => {
-    Aos.init({ duration: 2000 })
-  }, [])
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
-    <div className='bottomTop'>
-      <div data-aos="fade-right" className="bottomLeft">
-              <h1>
-                  ABOUT
-                  <br />
-                  WHITE TALES
-              </h1>
-              <p>
-                  Lorem ipsum dolor sit amet, consectetur
-                 
-                  adipiscing elit. Suspendisse mauris eros,
-                  iaculis placerat purus sed, euismod luctus
-                 
-                  quam. Curabitur quis bibendum augue.
-                 
-                  Nam convallis blandit sapien in molestie.
-                 
-                  Lorem ipsum dolor sit amet, consectetur
-                 
-                  adipiscing elit. Suspendisse mauris eros,
-                 
-                  iaculis placerat purus sed, euismod luctus
-                  quam. Curabitur quis bibendum augue.
-                 
-                  Lorem ipsum dolor sit amet, consectetur
-                 
-                  adipiscing elit. Suspendisse mauris eros,
-                 
-                  iaculis placerat purus sed, euismod luctus
-                  quam. Curabitur quis bibendum augue.
-              </p>
-          </div>
+    <div className="bottomTop">
+      <div   className="bottomLeft">
+        <h1>
+          ABOUT
+          <br />
+          WHITE TALES
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          mauris eros, iaculis placerat purus sed, euismod luctus quam.
+          Curabitur quis bibendum augue. Nam convallis blandit sapien in
+          molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Suspendisse mauris eros, iaculis placerat purus sed, euismod luctus
+          quam. Curabitur quis bibendum augue. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Suspendisse mauris eros, iaculis placerat
+          purus sed, euismod luctus quam. Curabitur quis bibendum augue.
+        </p>
+      </div>
       <div data-aos="fade-up" className="bottomRight"></div>
     </div>
-  )
+  );
 }
 
-export default BottomTop
+export default BottomTop;
