e0ecbd6121795ce47a154ac4d199e4b1498985cb
Jaspreet singh <jassingh0244@gmail.com>
2022-07-23 14:12:47 +0530

    video error fix

--- src/Component/Banner.js
+++ src/Component/Banner.js
@@ -6,7 +6,7 @@ import video from '../assets/Video/bannerVideo.mp4'
 function Banner() {
   let app = useRef(null);
 
-  const [scroll, setScroll] = useState();
+  const [scroll, setScroll] = useState(false);
 
   // useEffect(() => {
   //   window.addEventListener("scroll", (e) => {
@@ -15,20 +15,26 @@ function Banner() {
   //   });
   // }, []);
 
+
   useEffect(() => {
+    setScroll(true)
     gsap.to(".banner", { margin: "0 2%", x: 0, duration: 1, delay: 0.5 });
   }, []);
   return (
     <div className="banner">
-      {/* <div className="bannerImg" ref={(e) => (app = e)}>
+      <div className="bannerImg" ref={(e) => (app = e)}>
         <video height="100%" width="100%" autoplay='autoplay' loop muted='true' playsinline>
-          <source src={video} type="video/mp4" />
+          <source src='https://youtu.be/Ntb7Uw0JDto' type="video/mp4" />
         </video>
-      </div> */}
-      <div className="bannerImg" dangerouslysetinnerhtml={{
-        __html: `<video autoplay='autoplay' loop muted='true' playsinline><source
-        src=${video} type="video/mp4"
-      source></video>` }} ></div>
+      </div>
+      {scroll ? (
+        <div className="bannerImg" dangerouslySetInnerHTML={{
+          __html: `<video autoPlay ='autoplay' loop muted='true' playsinline><source
+        src=${video}
+      source></video>` }}></div>
+      ) : (
+        <h1>no video</h1>
+      )}
     </div>
   );
 }
